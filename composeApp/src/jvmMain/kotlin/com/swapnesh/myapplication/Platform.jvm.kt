9bffdcfffd73b3a4295251791bfc91d49791b166
Swapnesh <swapnesh7677@gmail.com>
2026-03-13 12:04:36 +0530

    update

--- composeApp/src/jvmMain/kotlin/com/swapnesh/myapplication/Platform.jvm.kt
+++
@@ -1,7 +0,0 @@
-package com.swapnesh.myapplication
-
-class JVMPlatform: Platform {
-    override val name: String = "Java ${System.getProperty("java.version")}"
-}
-
-actual fun getPlatform(): Platform = JVMPlatform()
